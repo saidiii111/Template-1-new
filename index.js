e33622418033b9c4224cb5939dd0412830b77322
saidiii111 <saidiii111@alsaghir.said@gmail.com>
2021-12-17 15:41:10 +0100

    testing

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+<script>
+function loadlang()
+  {
+  var lng = document.getElementById("langselector").value;
+  var cnt = document.getElementById("contents");
+  switch (lng)
+    {
+    case "en":
+      cnt.src = "https://jsfiddle.net/q2nw8o35/";
+    break;
+    case "fr":
+      cnt.src = "https://jsfiddle.net/jmn8c9tj/";
+    break;
+    }
+  }
+</script>
