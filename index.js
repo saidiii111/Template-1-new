a67dc203a264dbb037ecfb335329c49e8c10f334
saidiii111 <saidiii111@alsaghir.said@gmail.com>
2021-12-22 18:46:39 +0100

    beginning working on the responsivity

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ function myFunction() {
 }
 
 function getFlagEmoji(countryCode) {
-  const codePoints = countryCode
-    .toUpperCase()
-    .split("")
-    .map((char) => 127397 + char.charCodeAt());
-  return String.fromCodePoint(...codePoints);
+  return countryCode.toUpperCase()
+                    .replace(/./g, char => 
+      String.fromCodePoint(127397 + char.charCodeAt())
+  );
 }
 
 var language;
