7d55734821c10ccc7952c571e5276055f6f5aaee
saidiii111 <saidiii111@alsaghir.said@gmail.com>
2021-12-22 13:49:20 +0100

    test

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ function myFunction() {
   }
 }
 
+function getFlagEmoji(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
+}
+
 var language;
 function getLanguage() {
   localStorage.getItem("language") == null ? setLanguage("en") : false;
